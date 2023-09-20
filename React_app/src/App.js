3162b0b073f03d28640df192e8fc9f176824d8f0
vikrant chaudhary <115361534+vikrant7351@users.noreply.github.com>
2023-09-20 11:02:22 +0530

    Update App.js

--- React_app/src/App.js
+++ React_app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Registerpage from './component/Register';
 import LoginPage from './component/login';
+import Profile from './component/profile';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route path='/' element={ <LoginPage/>}/>
         <Route path='/Registerpage' element={<Registerpage/>}/>
+        <Route path='/Profile' element={<Profile/>}/>
 
     </Routes>
   </Router>
